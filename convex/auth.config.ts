bb1322c96c15087b97a2109825eb0620ab519c0d
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2024-06-19 12:00:35 -0300

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://smashing-hedgehog-30.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
